66400cbdb7db601eb53fca0dd93068a0fafa4a8f
Mytyl <39316039+alcremytyl@users.noreply.github>
2025-09-22 10:10:06 -0400

    work on waybar theme

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,12 +1,33 @@
 {
   enable = true;
   settings = [{
-    modules-left = ["hyprland/workspaces"];
-    modules-center = ["wayland/window"];
-    modules-right = ["clock" "battery"]; 
-
+    reload_on_style_change = true;
     position = "top";
 
+    modules-left = ["clock" "tray"];
+    modules-center = ["hyprland/workspaces"];
+    modules-right = ["group/expand" "network" "battery"]; 
+
+    "hyprland/workspaces" = {
+      format ="{icon}";
+      format-icons = {
+        active = "";
+        default = "";
+        empty = "";
+      };
+      persistent-workspaces = {
+        "*" = [1 2 3 4 5];
+      };
+    };
+
+    clock = {
+      tooltip-format = "<tt>{calendar}</tt>";
+      calendar.format.today = "<span color='#FAFBFC'<b>{}</b></span>";
+      actions = {
+        on-click-right = "shift_down";
+        on-click = "shift_up";
+      };
+    };
   }];
 
   style = builtins.readFile ./ellfouts.css;
