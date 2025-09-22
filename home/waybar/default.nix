8d4000ee96ca2394c7c32e733689550bc0443c67
Mytyl <39316039+alcremytyl@users.noreply.github>
2025-09-22 13:37:53 -0400

    waybar

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -28,6 +28,41 @@
         on-click = "shift_up";
       };
     };
+
+    network = {
+      format-wifi = " ";
+      format-ethernet = "󰈀 ";
+      format-disconnected =  " ";
+      tooltip-format-disconnected = "Disconnected";
+      tooltip-format-wifi = "{essid} ({signalStrength}%)";
+      tooltip-format-ethernet =  "{ifname} 󰈀 ";
+      on-click =  "kitty nmtui";
+    };
+
+    # currently disabled
+    bluetooth = {
+      format-on = "󰂯 ";
+      format-off = "󰂲 ";
+      format-disabled = "󰂲 ";
+      format-connected-battery = "󰥈 {device_battery_percentage}%";
+      tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+      tooltip-format-enumerate-connected = "{device_alias}\n{device_address}";
+      tooltip-format-enumerate-connected-battery = "{device_alias}\n{device_address}\n{device_battery_percentage}%";
+    };
+
+    battery = {
+      format = "{icon} {capacity}%";
+      format-charging = "󰂄 {capacity}%";
+      format-plugged = "󰂄 {capacity}%";
+      format-icons = [ "󰁻" "󰁼" "󰁾" "󰂀" "󰂂" "󰁹" ];
+
+      states = {
+        good = 95;
+        warning = 50;
+        critical = 30;
+      };
+    };
+
   }];
 
   style = builtins.readFile ./ellfouts.css;
