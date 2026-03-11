c2da6cf1e3270e29c489e516bdaf39ca2ef5d6f4
Mytyl <39316039+alcremytyl@users.noreply.github>
2026-03-11 11:12:40 -0400

    working on rofi theme finally

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,6 +1,19 @@
 {...}: {
   programs.rofi = {
     enable = true;
-    theme = "material";
+    modes = [ "drun" ];
+    # plugins = [ ];
+    # theme = "material";
+    theme = builtins.readFile "./config.rasi";
   };
 }
+
+/*
+https://github.com/dctxmei/rofi-themes
+
+applet
+- time, hover 
+- network
+
+
+*/
