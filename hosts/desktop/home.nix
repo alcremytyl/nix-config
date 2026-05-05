91e24c1a0cc8d2a2a5cc386d85fe07c7f1496d3b
alcremytyl <39316039+1SlenderGaming1@users.noreply.github.com>
2026-05-05 01:45:55 -0400

    finally fix workspaces on desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -21,8 +21,23 @@ in {
 
   programs.waybar.settings = [(base // overrides)];
 
-  wayland.windowManager.hyprland.settings.monitor = lib.mkForce [
-    "DP-2, 2560x1440@240, 0x0, 1"
-    "HDMI-A-1, 1920x1080@60, 2560x0, 1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = lib.mkForce [
+      "DP-2, 2560x1440@240, 0x0, 1"
+      "HDMI-A-1, 1920x1080@60, 2560x0, 1"
+    ];
+    # drawing tablet as space 10
+    workspace = [
+      "1,monitor:DP-2"
+      "2,monitor:DP-2"
+      "3,monitor:DP-2"
+      "4,monitor:DP-2"
+      "5,monitor:DP-2"
+      "6,monitor:DP-2"
+      "7,monitor:DP-2"
+      "8,monitor:DP-2"
+      "9,monitor:DP-2"
+      "10,monitor:HDMI-A-1"
+    ];
+  };
 }
