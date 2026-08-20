34d62d451d80a45b14c40fa8b63301a3f676ca8b
Mytyl <39316039+alcremytyl@users.noreply.github>
2026-08-20 15:44:33 -0400

    laptop drivers

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -7,4 +7,10 @@
   environment.systemPackages = with pkgs; [
     tree
   ];
+
+  hardware.tuxedo-drivers.enable = true;
+  hardware.tuxedo-rs = {
+    enable = true;
+    tailor-gui.enable = true;
+  };
 }
